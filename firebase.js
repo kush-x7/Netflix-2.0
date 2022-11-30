f495944e84ecb68a72ff0168af0b9bd0075d1d0a
kushagra <kushagratripathi7@gmail.com>
2022-11-30 16:57:05 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDupfZmnX-LxF-qAL8OoQTlGanZV6sINSI",
+  authDomain: "netflix-clone-b0636.firebaseapp.com",
+  projectId: "netflix-clone-b0636",
+  storageBucket: "netflix-clone-b0636.appspot.com",
+  messagingSenderId: "954388735798",
+  appId: "1:954388735798:web:6c68d6d53cc4d51e6a7597",
+};
